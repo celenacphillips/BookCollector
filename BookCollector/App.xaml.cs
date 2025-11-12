0955ac4bbc26e340eb64f204a580d2229c26fc89
Celena Phillips <celena.c.phillips@gmail.com>
2025-11-12 08:26:34 -0500

    Base MAUI project

---
+++ BookCollector/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BookCollector
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ BookCollector/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BookCollector
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
