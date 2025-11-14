c1a83340c0f6c31c0eff1f79a25aee791fb36bd0
Celena Phillips <celena.c.phillips@gmail.com>
2025-11-14 08:28:03 -0500

    Add Secondary and Tertiary colors and add more colors

--- BookCollector/Views/SettingsView.xaml.cs
+++ BookCollector/Views/SettingsView.xaml.cs
@@ -19,8 +19,6 @@ public partial class SettingsView : ContentPage
     public bool HiddenBooksOn { get; set; }
 
     // TO DO:
-    // Pick more colors - 11/12/2025
-    // Add secondary and tertiary color - 11/12/2025
     // Add other languages - 11/12/2025
     // Try to add color preview in the picker - 11/12/2025
 
@@ -29,12 +27,17 @@ public partial class SettingsView : ContentPage
         AppThemeList = [AppStringResources.Light, AppStringResources.Dark];
         SelectedAppTheme = Application.Current.UserAppTheme == AppTheme.Light ? AppThemeList[0] : AppThemeList[1];
 
-        ColorList = [AppStringResources.BlueGray, "Red"];
+        ColorList = [AppStringResources.BlueGray, "Red", "Purple", "Green", "Orange", "Teal", "Magenta"];
         var color = (Color)Application.Current.Resources["Primary"];
         var hexCode = color.ToHex().ToLower();
         SelectedColor = hexCode switch
         {
             "#ff0000" => ColorList[1],
+            "#751aff" => ColorList[2],
+            "#2db300" => ColorList[3],
+            "#b36b00" => ColorList[4],
+            "#248f8f" => ColorList[5],
+            "#b300b3" => ColorList[6],
             _ => ColorList[0],
         };
 
@@ -66,15 +69,31 @@ public partial class SettingsView : ContentPage
     void OnColorPickerSelectedIndexChanged(object sender, EventArgs e)
     {
         var picker = (Picker)sender;
-        var color = picker.SelectedItem.ToString();
-        string hexCode = color switch
+        var colorName = picker.SelectedItem.ToString();
+        string hexCode = colorName switch
         {
             "Red" => "ff0000",
+            "Purple" => "#751aff",
+            "Green" => "#2db300",
+            "Orange" => "#b36b00",
+            "Teal" => "#248f8f",
+            "Magenta" => "#b300b3",
             _ => "#336699"
         };
-        Application.Current.Resources["Primary"] = Color.FromArgb(hexCode);
+
+        var color = Color.FromArgb(hexCode);
+        var secondary = color.AddLuminosity((float)0.1);
+        var tertiary = color.AddLuminosity((float)0.2);
+
         CommunityToolkit.Maui.Core.Platform.StatusBar.SetColor(Color.FromArgb(hexCode));
+
+        Application.Current.Resources["Primary"] = Color.FromArgb(hexCode);
+        Application.Current.Resources["Secondary"] = Color.FromArgb(secondary.ToHex());
+        Application.Current.Resources["Tertiary"] = Color.FromArgb(tertiary.ToHex());
+
         Preferences.Set("AppColor", hexCode);
+        Preferences.Set("Secondary", secondary.ToHex());
+        Preferences.Set("Tertiary", tertiary.ToHex());
     }
 
     void OnLanguagePickerSelectedIndexChanged(object sender, EventArgs e)
